518d52767fd1b82f38977f33b3572d882525e2c9
cmnarrillos <cmorenonarrillos@gmail.com>
2023-10-08 12:38:45 +0200

    Task 3

---
+++ Tareas/Task_3/scripts/accuracy.m
@@ -0,0 +1,7 @@
+function y = accuracy(trueLabel,prediction, pos_val, neg_val)
+    TP = sum(trueLabel == pos_val & prediction == pos_val);
+    TN = sum(trueLabel == neg_val & prediction == neg_val);
+    FP = sum(trueLabel == neg_val & prediction == pos_val);
+    FN = sum(trueLabel == pos_val & prediction == neg_val);
+    y = (TP+TN)/(TP+TN+FP+FN);
+end
